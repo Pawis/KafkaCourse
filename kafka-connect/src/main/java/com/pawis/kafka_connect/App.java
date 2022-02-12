2da0e873260d40edaa8f2074c4ad6c26a0115b49
Pawis <Pawis88888@gmail.com>
2022-02-12 21:06:19 +0100

    kafka-connect

---
+++ kafka-connect/src/main/java/com/pawis/kafka_connect/App.java
@@ -0,0 +1,13 @@
+package com.pawis.kafka_connect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
